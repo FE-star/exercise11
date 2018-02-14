17c372d5e60b693504f00879dc01f75c92f5fac3
JaffarL <lyzandwow@163.com>
2018-02-14 09:49:58 +0800

    不知道这样对不对

--- webpack.config.js
+++ webpack.config.js
@@ -5,3 +5,38 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 // 这个在 npm run dev 和 npm run build 时候是不同的
 var TARGET = process.env.npm_lifecycle_event
 var APP_PATH = path.join(__dirname, '/src')
+console.log(TARGET)
+const config = {
+    entry: APP_PATH+'/index.js',
+    output:{
+        filename:'testbundle.js',
+        path: path.join(__dirname, '/test')
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use:ExtractTextPlugin.extract({
+                    fallback:'style-loader',
+                    use:[
+                        'css-loader'
+                    ]
+                })
+            }
+        ]
+    },
+    plugins:[
+        new ExtractTextPlugin("style.css")
+    ]
+}
+
+if(TARGET=='dev'){
+    console.log('dev')
+    config.module.rules=[
+        {
+                test: /\.css$/,
+                use:['style-loader','css-loader']
+        }
+    ]
+}
+module.exports = config
