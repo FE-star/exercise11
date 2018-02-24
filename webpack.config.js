35ff95bcfdfbe3d3fa2bce0d6d5c92c2b2188f75
王建忠 <yepbug@qq.com>
2018-02-24 11:07:44 +0800

    王建忠 webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,3 +5,38 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 // 这个在 npm run dev 和 npm run build 时候是不同的
 var TARGET = process.env.npm_lifecycle_event
 var APP_PATH = path.join(__dirname, '/src')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: TARGET === 'build'
+          ? ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [ 'css-loader' ]
+        })
+        : [ 'style-loader', 'css-loader' ],
+      },
+    ]
+  },
+  plugins: TARGET === 'build'
+  ? [
+      // 提取 css
+      new ExtractTextPlugin({
+        filename: '[name].css'
+      }),
+      // 压缩 js
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      }),
+    ]
+  : []
+}
