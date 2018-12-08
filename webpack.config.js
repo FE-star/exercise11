f8d39c98ea4ceabe5c29ac7cb1b03f0319ec43a4
machock <229235844@qq.com>
2018-12-08 20:17:57 +0800

    作业

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,34 @@
-var path = require('path')
-var webpack = require('webpack')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var path = require("path");
+var webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // 这个在 npm run dev 和 npm run build 时候是不同的
-var TARGET = process.env.npm_lifecycle_event
-var APP_PATH = path.join(__dirname, '/src')
+var TARGET = process.env.npm_lifecycle_event;
+var APP_PATH = path.join(__dirname, "/src");
+
+module.exports = {
+  entry: path.resolve(APP_PATH, "./index.js"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "./dist")
+  },
+  mode: TARGET === "start" ? "development" : "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          // replace ExtractTextPlugin
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
