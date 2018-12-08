374c035cc8882907f50ad2745039613e80f376e1
zhaopeng <719197352@qq.com>
2018-12-08 09:57:32 +0800

    work

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 var path = require('path')
 var webpack = require('webpack')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 // 这个在 npm run dev 和 npm run build 时候是不同的
 var TARGET = process.env.npm_lifecycle_event
 var APP_PATH = path.join(__dirname, '/src')
+
+module.exports = {
+    entry: path.resolve(APP_PATH, './index.js'),
+    mode: TARGET === 'build' ? 'production' : 'development',
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }],
+    },
+}
