3545ed446c55ea7d03adb56089d6b099d9efa408
Beace.li <Beace.li@trustasia.com>
2018-12-12 18:00:17 +0800

    feat: use webpack3.6 and webpack-dev-server 2.9.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,43 @@
-var path = require('path')
-var webpack = require('webpack')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // 这个在 npm run dev 和 npm run build 时候是不同的
-var TARGET = process.env.npm_lifecycle_event
-var APP_PATH = path.join(__dirname, '/src')
+var TARGET = process.env.npm_lifecycle_event;
+var APP_PATH = path.join(__dirname, '/src');
+var isProduction = TARGET !== 'start';
+
+const cssLoaders = isProduction
+  ? ExtractTextPlugin.extract({
+      use: ['css-loader'],
+      fallback: 'style-loader',
+    })
+  : ['style-loader', 'css-loader'];
+
+const plugins = isProduction
+  ? [
+      new ExtractTextPlugin('main.css'),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          properties: true,
+        },
+      }),
+    ]
+  : [];
+
+module.exports = {
+  entry: path.join(APP_PATH, './index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders,
+      },
+    ],
+  },
+  plugins,
+};
