e9e2fef8e49c2b3e3a2480764a24c53034af4967
panqiang <534047750@qq.com>
2019-04-21 10:17:21 +0800

    udpate

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,43 @@
 var path = require('path')
 var webpack = require('webpack')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 
 // 这个在 npm run dev 和 npm run build 时候是不同的
-var TARGET = process.env.npm_lifecycle_event
+var TARGET = process.env.npm_lifecycle_event;
+console.log(TARGET)
 var APP_PATH = path.join(__dirname, '/src')
+
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, use: [
+                    { loader: "style-loader" },
+                    {
+                        loader: "css-loader", options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false,
+            options: {
+                context: __dirname
+            }
+        })
+    ]
+}
