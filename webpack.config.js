1f5aabb379b02caed1ab2694d1e1f2d0caec0eb6
ldxldx <momobr@sina.cn>
2019-08-29 21:55:55 +0800

    success

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,28 @@ var webpack = require('webpack')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 // 这个在 npm run dev 和 npm run build 时候是不同的
-var TARGET = process.env.npm_lifecycle_event
-var APP_PATH = path.join(__dirname, '/src')
+var TARGET = process.env.npm_lifecycle_event;
+var APP_PATH = path.join(__dirname, '/src');
+var webpackConfig = {
+  entry: APP_PATH,
+  output: {
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: TARGET === 'start' ? [
+          'style-loader',
+          'css-loader'
+        ] : ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+    ]
+  },
+  plugins: []
+}
+if (TARGET === 'build') webpackConfig.plugins.push(new ExtractTextPlugin("styles.css"));
+module.exports = webpackConfig;
